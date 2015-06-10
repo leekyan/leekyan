8a4e290a85e1586ece50b27f965444bd1b72f40d
leekyan <yanlin826@163.com>
2015-06-10 23:14:21 +0800

    666

--- src/com/common/test/HelloWorld.java
+++ src/com/common/test/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
 		System.out.println("Hello World22222!!!");
 		System.out.println("Hello 33333!!!");
 		System.out.println("Hello 555!!!");
+		System.out.println("Hello 666!!!");
 	}
 }
